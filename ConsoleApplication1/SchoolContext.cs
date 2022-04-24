70e66d8f01e95e5778905de2b8295ab8dbee392c
Avinash <nannuriavinash@gmail.com>
2022-04-24 14:35:09 +0530

    Add project files.

---
+++ ConsoleApplication1/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Entity;
+
+namespace ConsoleApplication1
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+
+}
